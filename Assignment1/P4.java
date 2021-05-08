f57189655763aa2497a78be06e1509839e5cf9b5
Lakhane10 <83824277+Lakhane10@users.noreply.github.com>
2021-05-08 23:22:17 +0530

    Add files via upload

---
+++ Assignment1/P4.java
@@ -0,0 +1,34 @@
+//4. Write a Java program to print the result of the following operations. 
+//Test Data: 
+//a. -5 + 8 * 6
+//b. (55+9) % 9
+//c. 20 + -3*5 / 8
+//d. 5 + 15 / 3 * 2 - 8 % 3
+//Expected Output :
+//43
+//1
+//19
+//13
+
+class P4
+{
+ public static void main(String args[])
+   {
+     int a,b,c,d=0;
+	 a= -5 +8 *6;
+	 b= (55+9) % 9;
+	 c= 20 + -3*5 / 8;
+	 d= 5+ 15 /3 *2 -8 %3;
+	 System.out.println("a = "+a);
+	  System.out.println("b = "+b);
+	   System.out.println("c = "+c);
+	    System.out.println("d = "+d);
+    }
+}
+	/*
+output: java Assignment4
+a = 43
+b = 1
+c = 19
+d = 13
+*/	
